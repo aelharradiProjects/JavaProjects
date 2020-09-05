609e8a7ddec53512103289d7facc9807bad0324d
aelharradi.projects@gmail.com <aelharradi.projects@gmail.com>
2020-09-05 13:12:00 +0100

    no message

--- Printer/src/Main.java
+++ Printer/src/Main.java
@@ -1,6 +1,19 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("test");
+		int numberPages = 3;
+		int addedPages=0;
+		if (numberPages%4 != 0) {
+			addedPages=4-numberPages%4;
+			numberPages=numberPages+addedPages;
+		}
+		System.out.println(addedPages + " Pages added ");
+		for (int i = 1; i <= numberPages/2; i+=2) {
+			System.out.print(i+",");
+			System.out.print(numberPages/2+i+",");
+			System.out.print(numberPages/2+i+1+",");
+			System.out.print(i+1+",");
+		}
+		
 	}
 }
